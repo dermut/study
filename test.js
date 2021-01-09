4e654e56841f6a3ac39672a62008d36cdf652535
Jimin Kim <ultimate1994@naver.com>
2021-01-09 23:11:13 +0900

    study

---
+++ test.js
@@ -0,0 +1,11 @@
+var shoppingCart = {
+    items: ['Aple', 'Orange'],
+    inventory: { Apple: 1, Orange: 0 },
+    checkout: () => {
+        this.items.forEach(item => {
+            if (!this.inventory[item]) {
+                console.log('Item ' + item + ' has sold out.');
+            }
+        }) 
+    }
+}
