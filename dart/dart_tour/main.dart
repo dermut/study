8c98e5ee270a0fa3d6577bd17316509207a48cde
dermut <ultimate1994@naver.com>
2024-09-29 12:10:06 +0900

    Optional Positional Parameter

--- dart/dart_tour/main.dart
+++ dart/dart_tour/main.dart
@@ -1,28 +1,8 @@
-// String sayHello(String name) => "Hello $name nice to meet you!";
-String sayHello(String name) {
-  // call api
-  // perform calc
-  return "Hello $name nice to meet you!";
-}
-
-// NamedParameter를 사용하기 위해 Default Value를 지정한 방법
-// String sayHelloNamed({
-//   String name = 'anon',
-//   int age = 99,
-//   String country = 'wakanda',
-// }) {
-//   return "Hello $name, you are $age, and you come from $country";
-// }
-
-// NamedParameter를 사용하기 위해 required로 지정한 방법
-String sayHelloNamed({
-  required String name,
-  required age,
-  required String country,
-}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
+// Optional Positional Parameter
+String sayHello(String name, int age, [String? country = 'cuba']) =>
+    'Hello $name, you are $age years old from $country';
 
 void main() {
-  print(sayHelloNamed(age: 12, country: 'cuba', name: 'jimin'));
+  var results = sayHello('jimin', 2);
+  print(results);
 }
