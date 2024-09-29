bfd0ca43fddd97485a3ce76d48cd8d7d4a0ddf68
dermut <ultimate1994@naver.com>
2024-09-29 11:13:09 +0900

    String Interpolation

--- dart/dart_tour/main.dart
+++ dart/dart_tour/main.dart
@@ -1,11 +1,18 @@
 void main() {
-  var giveMeFive = false;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5,
+  var name = 'jimin';
+  var age = 30;
+  var greeting =
+      "Hello everyone, my name is $name and I'm ${age + 2}, nice to meet you!";
+  print(greeting);
+
+  var oldFriends = [
+    'jimin',
+    'seyoung',
   ];
-  print(numbers);
+  var newFriends = [
+    'simba',
+    'haku',
+    for (var friend in oldFriends) "💘 $friend"
+  ];
+  print(newFriends);
 }
