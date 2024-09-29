968b49cbc8e8a2f6e58c5b3cb225d2f366ffdbfd
dermut <ultimate1994@naver.com>
2024-09-29 12:48:30 +0900

    클래스의 Named, Positional Contsturctor

--- dart/dart_tour/main.dart
+++ dart/dart_tour/main.dart
@@ -10,6 +10,22 @@ class Player {
     required this.age,
   });
 
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(
+    String name,
+    int age,
+  )   : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
   void sayHello() {
     // Dart에서는 this를 사용하지 않는 것을 권장함.
     // 만약 sayHello에 겹치는 이름이 있으면 this를 사용할 필요는 있음.
@@ -19,18 +35,14 @@ class Player {
 }
 
 void main() {
-  var player = Player(
+  var player = Player.createBluePlayer(
     name: "jimin",
-    xp: 1500,
-    team: 'red',
     age: 12,
   ); // new는 필수가 아니며, 이렇게하면 인스턴스가 생성됨
   player.sayHello();
-  var player2 = Player(
-    name: "lynn",
-    xp: 2500,
-    team: 'blue',
-    age: 13,
+  var player2 = Player.createRedPlayer(
+    "lynn",
+    13,
   );
   player2.sayHello();
 }
