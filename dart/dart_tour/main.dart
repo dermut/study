abdaa10e23fce5ea9c1f24c84508fd61c619f035
dermut <ultimate1994@naver.com>
2024-09-29 12:42:31 +0900

    원초적으로 Class를 만드는 방법

--- dart/dart_tour/main.dart
+++ dart/dart_tour/main.dart
@@ -1,18 +1,36 @@
-// 이 방법으로도 Nullable 처리가 가능하다
-// String capitalizeName(String? name) {
-//   if (name != null) {
-//     return name.toUpperCase();
-//   }
-//   return 'ANON';
-// }
+class Player {
+  final String name;
+  int xp, age;
+  String team;
 
-String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANON';
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  void sayHello() {
+    // Dart에서는 this를 사용하지 않는 것을 권장함.
+    // 만약 sayHello에 겹치는 이름이 있으면 this를 사용할 필요는 있음.
+    // this.name;
+    print("Hi my name is $name");
+  }
+}
 
 void main() {
-  capitalizeName('jimin');
-  capitalizeName(null);
-
-  String? name;
-  name ??= 'jimin';
-  print(name);
+  var player = Player(
+    name: "jimin",
+    xp: 1500,
+    team: 'red',
+    age: 12,
+  ); // new는 필수가 아니며, 이렇게하면 인스턴스가 생성됨
+  player.sayHello();
+  var player2 = Player(
+    name: "lynn",
+    xp: 2500,
+    team: 'blue',
+    age: 13,
+  );
+  player2.sayHello();
 }
