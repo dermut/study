0ee0cd344bbfde6b6a4a92397aea69aedadede04
dermut <ultimate1994@naver.com>
2024-10-07 21:46:09 +0900

    Hello World 스타일 조정

--- flutter/toonflix/lib/main.dart
+++ flutter/toonflix/lib/main.dart
@@ -8,12 +8,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Hello flutter'),
-            ),
-            body: Center(
-              child: Text('Hello World!'),
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
   }
 }
