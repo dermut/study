e8c51376f0515115ad6c35f221e062f694b0bd24
dermut <ultimate1994@naver.com>
2024-10-08 22:50:41 +0900

    UI 따라그리기

--- flutter/toonflix/lib/main.dart
+++ flutter/toonflix/lib/main.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String name;
+
+  Player({required this.name});
+}
+
 void main() {
+  var jimin = Player(name: 'jimin');
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey Selina',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
